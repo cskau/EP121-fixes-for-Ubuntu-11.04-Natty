6c39ef21edc6b51cd7d38cc6bbe272a6606cf974
Clement Skau <clementskau@gmail.com>
2011-05-19 17:19:21 +0200

    Fixes a bunch of bad BASH scripting..

--- ep121.sh
+++ ep121.sh
@@ -16,18 +16,21 @@ echo "bluez hold" | sudo dpkg --set-selections
 ## Fix touch screen
 
 # install "driver" in user's bin folder
-wget ep121_drv.py ~/bin/
+wget --no-check-certificate https://github.com/cskau/EP121-fixes-for-Ubuntu-11.04-Natty/raw/master/ep121_drv.py -P ~/bin/
 # grant the driver read access to input devices
-echo "SUBSYSTEM==\"input\", MODE=\"644\"" > sudo /etc/udev/rules.d/85-ep121.rules
+echo "SUBSYSTEM==\"input\", MODE=\"644\"" | sudo tee -a /etc/udev/rules.d/85-ep121.rules
 # make sure the driver runs at start up (log in)
 echo "ep121_drv.py &" >> ~/.profile
 
 
 ## Add hot keys
 
+# We might want to fiddle with this at some point
+# would be nice to get proper keybindings for them
+
 # Rotation
-#echo "0xF5 prog1" >> sudo /lib/udev/keymaps/asus
+#echo "0xF5 prog1" >> /lib/udev/keymaps/asus
 # Screen
-#echo "0xF6 f21 # Toggle touchpad" >> sudo /lib/udev/keymaps/asus
+#echo "0xF6 f21 # Toggle touchpad" >> /lib/udev/keymaps/asus
 # Keyboard
-#echo "0xF7 bluetooth" >> sudo /lib/udev/keymaps/asus
+#echo "0xF7 bluetooth" >> /lib/udev/keymaps/asus
